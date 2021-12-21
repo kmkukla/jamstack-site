6f396ba7f8caa831766432f7cba7ff8a8f383846
Konrad Kukla <kondziocard@gmail.com>
2021-12-21 09:20:22 +0100

    create serverless contact form

---
+++ contact/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../components/form';
+import '../global.css';
+
+const App = () => (
+  <>
+    <h1>Send a message</h1>
+    <Form />
+  </>
+);
+
+export default App;
